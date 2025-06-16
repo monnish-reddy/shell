345ce758579639aa187001a49da322437cfa99c0
root <root@ip-172-31-92-162.ec2.internal>
2025-06-16 00:20:22 +0000

    specia variables

---
+++ specialvariables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo " hi to pass all arguments $@"
+echo " to find count of arguments $#"
+echo " script name $0"
+echo " present working directory $PWD"
+echo " home of user $HOME"
+echo "user is $USER"
+echo " procces is of current is  $$"
+echo "executed code is $?"
