fb05a4cf4dd8a346576139a0826b1549e3de1bff
monnish-reddy <peta.monnish@gmail.com>
2025-06-19 21:35:50 -0400

    latest

--- specialvariables.sh
+++ specialvariables.sh
@@ -4,6 +4,6 @@ echo "the current user $USER"
 echo " the pid o fthr script $$"
 echo " the current directory $PWD"
 echo "the home  directory $HOME"
-sleep 10 
+sleep 10 &
 echo " the pid of last command in backgroung $!"
 echo "the count of args passed to script :$#"
