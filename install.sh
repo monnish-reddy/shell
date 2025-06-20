5c8a91350ac47a0b4b8b24dd8627970a3bbbbd75
monnish-reddy <peta.monnish@gmail.com>
2025-06-19 21:46:09 -0400

    latest

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ];
+then 
+    echo "ERROR: Please run the script with root access"
+    exit 1
+else
+    echo "You are running with root access"
+    dnf install mysql -y
