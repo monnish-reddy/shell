ddb99685145105c9679d644f48a4f5edb71eedee
monnish-reddy <peta.monnish@gmail.com>
2025-06-19 21:29:08 -0400

    latest

--- specialvariables.sh
+++ specialvariables.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 echo "all variables passed in script are : $@"
+echo "the current user $USER"
+echo " the pid o fthr script $$"
+echo " the current directory $PWD"
+echo "the home  directory $HOME"
+sleep 10 
+echo " the pid of last command in backgroung $!"
+echo "the count of args passed to script :$#"
